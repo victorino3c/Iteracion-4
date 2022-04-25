e1162817b74686741100004edd7e0c8cd7c61c40
Pingu9191 <99191235+Pingu9191@users.noreply.github.com>
2022-04-25 20:20:59 +0200

    Update command.h

--- include/command.h
+++ include/command.h
@@ -44,7 +44,8 @@ typedef enum enum_Command
   ATTACK,       /*!< Command type is to attack an enemy. Id of the enemy must be included as a second argument. */
   MOVE,         /*!< Command type is to move player (direction specify in another argument). */
   INSPECT,      /*!< Command type is to inspect. */
-  SAVE          /*!< Command type is to save (name of the file as a second argument). */
+  SAVE,         /*!< Command type is to save (name of the file as a second argument). */
+  LOAD          /*!< Command type is to load (name of the file as a second argument). */
 } T_Command;
 
 /**
