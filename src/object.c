61325364427601bda82476761565ad15e381ada6
victorino3c <99192011+victorino3c@users.noreply.github.com>
2022-04-26 10:02:14 +0200

    Update object.c

--- src/object.c
+++ src/object.c
@@ -424,7 +424,7 @@ STATUS obj_print_save(char *filename, Object *obj)
 
 /** Return true if the object is visible
 */
-BOOL obj_is_visible(Object *obj, Time time, Light ls)
+BOOL obj_is_visible(Object *obj, Light ls)
 {
   if (obj == NULL || ls == UNKNOWN_LIGHT)
   {
