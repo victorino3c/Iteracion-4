1573881b44892d526c2dae504bee56e6905ede73
victorino3c <99192011+victorino3c@users.noreply.github.com>
2022-05-08 19:29:28 +0200

    Update partida2.cmd

--- partida2.cmd
+++ partida2.cmd
@@ -1,11 +1,31 @@
-s
-s
-d
-d
-d
-d
-i s
-c Hoja
-i Hoja
-v Hoja
+move s
+move s
+move n
+move n
+take torch1
+inspect s
+inspect torch1
+ton torch1
+drop torch1
+move s
+move s
+move e
+move e
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+take key2
+open Door2 with key2
+move e
 exit
